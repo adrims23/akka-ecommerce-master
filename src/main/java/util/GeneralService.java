234513af525403bbc440986266faf9c655cec1d7
spaul <spau10@sapient.com>
2018-06-11 17:11:06 +0530

    Populating exception map

--- src/main/java/util/GeneralService.java
+++ src/main/java/util/GeneralService.java
@@ -9,6 +9,7 @@ import java.util.Map;
 public class GeneralService {
     public static String sendErrorJson(Exception e) {
         Map<String, String> exceptionMap = new HashMap<>();
+        exceptionMap.put("There was an issue with your request",e.getMessage());
         return JSONParser.quote(exceptionMap.toString());
     }
 }
